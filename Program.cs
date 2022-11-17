f17242ecc316726413996fdd4592b193d6ff89d8
nidiakk <73993844+nidiakk@users.noreply.github.com>
2022-11-17 22:38:28 +0800

    亂數產生自由落體的秒數

--- Program.cs
+++ Program.cs
@@ -2,111 +2,60 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
-namespace vs2022_4_1
+using System.Threading.Tasks;
+
+namespace CS2022_10_4_2_
 {
     internal class Program
-    {   static Thread threadA, threadB, threadC;
-        static Random RN = new Random();
-        static byte A, B, C, Nth;
-        static int SleepTime;
-
+    {
         static void Main(string[] args)
-        {
+        {   Random rn = new Random();
             while (true)
             {
+                double h = 0;
+                double t = 0;
+                double g = 9.80665;
                 try
                 {
-                    Console.Write("Press Y(y) for Continue Else for No？ Nth？ Max SleepTime(ms)？");
-                    string[]Line=Console.ReadLine().Split(' ');
-                    Nth = Byte.Parse(Line[1]);//Byte:0~255
-                     SleepTime=int.Parse(Line[2]);
-                    if (Line[0] == "y" || Line[0] == "Y")
+                    checked
                     {
-                        A=0; B=0; C=0;
-                        threadA = new System.Threading.Thread(new System.Threading.ThreadStart(IMA));
-                        threadB = new Thread(new ThreadStart(IMB));
-                        threadC = new Thread(new ThreadStart(IMC));
-                        threadA.Start();
-                        threadB.Start();
-                        threadC.Start();
+                        
+                        //1.
+                        //Console.Write("Input 自由落體之秒數(Second)=?");
+                        //t = double.Parse(Console.ReadLine());
+                        //2.
+                        Console.WriteLine(System.DateTime.Now);
+                        t= 10*rn.NextDouble();
+
+                        Console.Write("Input 自由落體之秒數(Second)=?"+Math.Round(t,1));
+                        h = (1.0 / 2.0) * g *Math.Pow(t,2);
+
+                        Console.BackgroundColor= ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("   吊橋高度(h)="+Math.Round(h,2)+"公尺");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        System.Threading.Thread.Sleep(rn.Next(500,5001));
+                        //System.Threading.Thread.Sleep(rn.Next(2000));
+
                     }
-                    else return;
                 }
-           catch (Exception ex)
+                catch (Exception ex)
                 {
-                    Console.WriteLine(ex.ToString());
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex);//顯示錯誤提示
+                    Console.ForegroundColor = ConsoleColor.White;
                 }
                 finally
                 {
-                    Console.ReadKey();
+                    Console.WriteLine("Try again !!");
+                    Console.WriteLine();
+                    //Console.ReadKey();
                 }
-             }
-        }
-        
-        private static void IMA()
-        {
-            while (true)
-            {
-                System.Threading.Thread.Sleep(RN.Next(SleepTime));
-                A++;
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine("資一甲("+A+"):"+System.DateTime.Now);
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine();
-                if (A == Nth)
-                {
-                    threadB.Abort(); threadC.Abort();
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine("Winner：資一甲");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    threadA.Abort();
-                    
-                }
-                
             }
-        }
 
-        private static void IMB()
-        {
-            while (true)
-            {
-                Thread.Sleep(RN.Next(SleepTime));
-                B++;
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("資一乙(" + B + "):" + System.DateTime.Now.ToLongTimeString().ToString());
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine();
-                if (B == Nth)
-                {
-                    threadA.Abort(); threadC.Abort();
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Winner：資一乙" );
-                    Console.ForegroundColor = ConsoleColor.White;
-                    threadB.Abort();
-                }
-            }
-        }
-        private static void IMC()
-        {
-            while (true)
-            {
-                Thread.Sleep(RN.Next(SleepTime));
-                C++;
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("資一丙(" + C + "):" + System.DateTime.Now.Hour+ ":"+DateTime.Now.Minute+ ":"+DateTime.Now.Second);
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine();
-                if (C == Nth)
-                {
-                    threadA.Abort(); threadB.Abort();
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Winner：資一丙");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    threadC.Abort();
-                }
-            }
+
         }
     }
 }
